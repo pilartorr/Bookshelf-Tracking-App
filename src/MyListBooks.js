4dccc9926512d2bad49efc316d2d8c3fbe1f8418
pilartorr <sauleturme@gmail.com>
2020-03-24 16:31:16 +0100

    created Book Component

--- src/MyListBooks.js
+++ src/MyListBooks.js
@@ -1,5 +1,6 @@
 import React,  { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class MyListBooks extends Component {
     state = {
@@ -21,7 +22,6 @@ class MyListBooks extends Component {
               name: 'Read'
             }
         ];
-        //const options = [ "Move to...", "Currently Reading", "Want to Read", "Read", "None"];
 
         return(
             <div className="list-books">
@@ -39,22 +39,7 @@ class MyListBooks extends Component {
                                             if(book.shelf === bookshelf.key) {
                                                 return (
                                                     <li key={book.id}>
-                                                        <div className="book">
-                                                            <div className="book-top">
-                                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                                <div className="book-shelf-changer">
-                                                                <select>
-                                                                    <option value="move" disabled>Move to...</option>
-                                                                    <option value="currentlyReading">Currently Reading</option>
-                                                                    <option value="wantToRead">Want to Read</option>
-                                                                    <option value="read">Read</option>
-                                                                    <option value="none">None</option>
-                                                                </select>
-                                                                </div>
-                                                            </div>
-                                                            <div className="book-title">{book.title}</div>
-                                                            <div className="book-authors">{book.authors}</div>
-                                                        </div>
+                                                        <Book book={book} />
                                                     </li>
                                                 )
                                             }
