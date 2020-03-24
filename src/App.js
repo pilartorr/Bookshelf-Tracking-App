0af6f29f9e71a08605ffef9923ba0a18bc6b534a
pilartorr <sauleturme@gmail.com>
2020-03-24 13:27:37 +0100

    getting all books of BookAPI

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import { BrowserRouter, Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class App extends Component {
   state = {
-    books: []
+    mybooks: []
   }
-
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }),
+        () => {
+          console.log(this.state);
+        })
+      })
+  }
   render() {
     return (
       <div className="app">
